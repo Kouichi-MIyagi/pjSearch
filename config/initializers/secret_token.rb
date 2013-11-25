709a415b26bef2a8d0fdd7e808de8f2835b77b7d
unknown <P9572843@P9572843B.ad.ogis-ri.co.jp>
2013-11-25 21:38:35 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PjSearch::Application.config.secret_token = '206a3f28ea185b5e0df84c36bac75314dd7b664d827d47c5eb3f7553d76cee8db7626e5f3f415494c37d6158f88146624db1a4f98c4702df86cba6b632f22594'
