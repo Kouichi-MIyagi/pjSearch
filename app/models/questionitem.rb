a0573b41c7467b7e2075c88914f3c22c861ab675
ttamura204 <P8971228@tayori.ogis-ri.co.jp>
2013-12-03 16:21:49 +0900

    アンケートひな形と質問の画面を作成

--- app/models/questionitem.rb
+++ app/models/questionitem.rb
@@ -1,3 +1,5 @@
 class Questionitem < ActiveRecord::Base
-  attr_accessible :id, :question, :questionItem, :selectionNumber
+  attr_accessible :id, :question, :answer1, :answer1_item, :answer2, :answer2_item, :answer3, :answer3_item, :answer4, :answer4_item
+  
+  has_and_belongs_to_many :questionnaires
 end
