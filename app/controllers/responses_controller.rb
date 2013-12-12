e582cfe24d44ea16de11647b9a718f600c911b9b
ttamura204 <P8971228@tayori.ogis-ri.co.jp>
2013-12-12 13:09:50 +0900

    install will_paginate

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -3,20 +3,17 @@ class ResponsesController < ApplicationController
   # GET /responses.json
   def index
 
-    @searched = Hash.new()
-    session[:searched] = @searched
-	
     if params[:request_id].nil?    
-    #  @responses = Response.all
-	  if current_user.isAdmin?
-	    # 管理者は全て表示する
+    # アンケート依頼に対する回答一覧以外（通常はこちら）
+      if current_user.isAdmin?
+	  # 管理者は全て表示する
 	    @responses = Response.all
 	  else
-	    # ログインしたユーザーの回答一覧
+	  # ログインしたユーザーの回答一覧
         @responses = Response.where("user_id = ?", current_user.id)
 	  end
     else
-	  # アンケート依頼に対する回答一覧
+	# アンケート依頼に対する回答一覧
       request = RequestQuestionnaire.find(params[:request_id])
       if request
         @responses = request.responses
@@ -24,28 +21,41 @@ class ResponsesController < ApplicationController
         @responses = Array new
       end
     end
-	
-	if !params[:search].nil?
-	# 検索ボタン押下時：画面入力された条件のセッションへの保存
-      params[:search].each do | key, value |
-      @searched.store(key, value)
-      end
+
+    if params[:page].nil?
+    # 検索条件の設定
+    # ページ繰り以外
+      @searched = Hash.new()
+      session[:searched] = @searched
+	  	
+	  if !params[:search].nil?
+	  # 検索ボタン押下時：画面入力された条件のセッションへの保存
+        params[:search].each do | key, value |
+        @searched.store(key, value)
+        end
+	  end
+	  	
+	else
+	  # ページ繰り時：検索条件のセッションからの取り出し
+      @searched = session[:searched]
 	end
-		
+	
+	# まずはページングを指示
+    @responses = Response.paginate(:page => params[:page], :per_page => 10)
+	
 	# 検索条件が指定されていれば、抽出条件としてwhere句を追加
     # プロジェクト名
     if !(@searched.fetch('pjName', nil).blank?)
-      @responses = Response.where('responses.pjName like ?', "%" + @searched.fetch('pjName')+ "%")
+      @responses = @responses.where('responses.pjName like ?', "%" + @searched.fetch('pjName')+ "%")
     end
     # 対象年
     if !(@searched.fetch('targetYear', nil).blank?)
-      @responses = Response.where('responses.targetYear = ?', @searched.fetch('targetYear'))
+      @responses = @responses.where('responses.targetYear = ?', @searched.fetch('targetYear'))
     end
     # 対象月
     if !(@searched.fetch('targetMonth', nil).blank?)
-      @responses = Response.where('responses.targetMonth = ?', @searched.fetch('targetMonth'))
+      @responses = @responses.where('responses.targetMonth = ?', @searched.fetch('targetMonth'))
     end
-
 
     respond_to do |format|
       format.html # index.html.erb
