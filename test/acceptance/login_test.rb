6531d744682a2c37ff69e68326c3367e3e3e9236
tamura <tamura_tetsuya@ogis-ri.co.jp>
2014-08-27 11:22:03 +0900

    一般ユーザーでのログインテスト追加

--- test/acceptance/login_test.rb
+++ test/acceptance/login_test.rb
@@ -3,8 +3,8 @@
 class LoginTest <AcceptanceTest
   #fixtures :users
   
-   # [正常系] 妥当なユーザ情報を入力してサインアップ -> サインアウト
-  test "sign up with valid user information and then sign out" do
+   # [正常系] 管理者でサインアップ -> サインアウト
+  test "sign up with admin and then sign out" do
     visit_root
     ensure_browser_size
     
@@ -22,8 +22,30 @@ class LoginTest <AcceptanceTest
 
     sign_out
 
+	end	
+	
+	# [正常系] 一般ユーザでサインアップ -> サインアウト
+  test "sign up with user and then sign out" do
+    visit_root
+    ensure_browser_size
+    
+    save_screenshot "scenario-02-01.png" 
+      
+      fill_in "user_user_id", with: "p8971228"
+      fill_in "user_password", with: "password"
+      save_screenshot "scenario-02-02.png" 
+
+      click_button "サインイン"
+      save_screenshot "scenario-02-03.png" 
+
+      # 適切な画面に遷移したかを確認
+      assert_equal new_response_path, current_path
+
+    sign_out
+
 	end
-  # [異常系] ユーザ ID の桁は８桁だがすべて数字でサインアップ
+
+  # [異常系] ユーザーIDとパスワード不整合
   test "try to sign up without password" do
     visit_root
     ensure_browser_size
@@ -31,6 +53,7 @@ class LoginTest <AcceptanceTest
       save_screenshot "scenario-03-01.png" 
 
       fill_in "user_user_id", with: "pjsadmin"
+      fill_in "user_password", with: "pjsadmin"
       save_screenshot "scenario-03-02.png" 
 
       click_button "サインイン"
@@ -40,5 +63,5 @@ class LoginTest <AcceptanceTest
       assert_equal user_session_path, current_path
   end
 
-
+	
 end
